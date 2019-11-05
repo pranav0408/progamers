9460bd123cb8733e7a72b2b96154b10c274d9dd4
Pranav Singh <zeus811@pop-os.localdomain>
2019-11-05 10:50:19 +0530

    Added Articles related pages

--- index.js
+++ index.js
@@ -8,24 +8,42 @@ const PORT = process.env.PORT || 8000
 // body parser middleware
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
+
+// setting static serving
 app.use( express.static(path.join(__dirname,'public')) )
 
 // Handlebars Middleware
 app.engine('handlebars', exphbs({ defaultLayout: 'main'}))
 app.set('view engine','handlebars')
 
+// routing to home page
 app.get('/',(req,res)=>{
     res.sendFile(path.join(__dirname,'public','index.html'))
 })
 
+// routing to trend page
 app.get('/trend',(req,res)=>{
     res.sendFile(path.join(__dirname,'public','trend.html'))
 })
 
+// routing to article container page
 app.get('/articles',(req,res)=>{
-    res.send("<h1>Article Page</h1>")
+    res.sendFile(path.join(__dirname,'public/article','index.html'))
+})
+// routing to call of duty article page
+app.get('/cod',(req,res)=>{
+    res.sendFile(path.join(__dirname,'public/cod','index.html'))
+})
+// routing to god of war article page
+app.get('/godofwar',(req,res)=>{
+    res.sendFile(path.join(__dirname,'public/god_of_war','index.html'))
+})
+// routing to fortnite article page
+app.get('/fortnite',(req,res)=>{
+    res.sendFile(path.join(__dirname,'public/fortnite','index.html'))
 })
 
+// routing to login page
 app.get('/login',(req,res)=>{
     res.sendFile(path.join(__dirname,'public','login.html'))
 })
@@ -33,6 +51,7 @@ app.get('/login',(req,res)=>{
 // Profile page
 app.use('/api/person', require('./Routes/api/profile'))
 
+// display profile page
 app.get('/profile/:id',(req,res)=>{
 
     const found = person.some( person => person.id == req.params.id )
@@ -51,6 +70,8 @@ app.get('/profile/:id',(req,res)=>{
         bio: profile[0].bio
     })
 })
+
+// profile update page
 app.get('/profile/update/:id',(req,res)=>{
 
     const found = person.some( person => person.id == req.params.id )
